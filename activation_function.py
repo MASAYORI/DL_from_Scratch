e69cda51c92b19c13bfe499893a8873be3f6485a
MASAYORI <o.massa1117@gmail.com>
2022-04-30 15:38:08 +0900

    Implementation of logic circuits using perceptron-like methods

---
+++ activation_function.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pylab as plt
+
+
+def step_function(x):
+    return (x > 0).astype(np.int32)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def main():
+    x = np.arange(-5.0, 5.0, 0.1)
+    y1 = step_function(x)
+    y2 = sigmoid(x)
+    y3 = relu(x)
+    plt.plot(x, y1, label='Step')
+    plt.plot(x, y2, label='Sigmoid')
+    plt.plot(x, y3, label='ReLU')
+    plt.ylim(-0.1, 1.1)
+    plt.legend()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
