41e157a7ab2fc643da5ebbd49740813e873dd412
MASAYORI <o.massa1117@gmail.com>
2022-05-06 00:57:53 +0900

    first commit

---
+++ gradient_simplenet.py
@@ -0,0 +1,34 @@
+from activation_function import *
+from loss_function import *
+from numerical_diff import *
+
+
+class SimpleNet:
+    def __init__(self):
+        self.W = np.random.randn(2, 3)
+
+    def predict(self, x):
+        return np.dot(x, self.W)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        loss = cross_entropy_error(y, t)
+
+        return loss
+
+
+def main():
+    net = SimpleNet()
+    print(net.W)
+    x = np.array([0.6, 0.9])
+    p = net.predict(x)
+    print(p)
+    print(np.argmax(p))
+    t = np.array([0, 0, 1])
+    print(net.loss(x, t))
+
+
+if __name__ == '__main__':
+    main()
+
