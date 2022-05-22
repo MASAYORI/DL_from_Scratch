6a8840e1f8f65f9ce56f45ea77ea7aef09f9a2f1
MASAYORI <o.massa1117@gmail.com>
2022-05-22 15:33:25 +0900

    update accuracy function

--- two_layer_net.py
+++ two_layer_net.py
@@ -12,22 +12,28 @@ class TwoLayerNet:
                  hidden_size,
                  output_size,
                  weight_init_std=0.01):
-        self.params = {'W1': weight_init_std * \
-                             np.random.randn(input_size, hidden_size),
+
+        # 重みの初期値: Heの初期値
+        self.params = {'W1': np.random.randn(input_size, hidden_size)/np.sqrt(input_size/2),
                        'b1': np.zeros(hidden_size),
-                       'W2': weight_init_std * \
-                             np.random.randn(hidden_size, output_size),
+                       'W2': np.random.randn(hidden_size, output_size)/np.sqrt(hidden_size/2),
                        'b2': np.zeros(output_size)}
+        # self.params = {'W1': weight_init_std * \
+        #                      np.random.randn(input_size, hidden_size),
+        #                'b1': np.zeros(hidden_size),
+        #                'W2': weight_init_std * \
+        #                      np.random.randn(hidden_size, output_size),
+        #                'b2': np.zeros(output_size)}
 
         self.layers = OrderedDict()
         self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
-        self.layers['ReLu'] = Relu()
+        self.layers['ReLu1'] = Relu()
         self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
         self.lastLayer = SoftmaxWithLoss()
 
     def predict(self, x):
-        for layers in self.layers.values():
-            x = layers.forward(x)
+        for layer in self.layers.values():
+            x = layer.forward(x)
         return x
 
     def loss(self, x, t):
@@ -37,7 +43,8 @@ class TwoLayerNet:
     def accuracy(self, x, t):
         y = self.predict(x)
         y = np.argmax(y, axis=1)
-        t = np.argmax(t, axis=1)
+        if t.ndim != 1:
+            t = np.argmax(t, axis=1)
 
         accuracy = np.sum(y == t) / float(x.shape[0])
         return accuracy
@@ -45,10 +52,11 @@ class TwoLayerNet:
     def numerical_gradient(self, x, t):
         loss_W = lambda W: self.loss(x, t)
 
-        grads = {'W1': numerical_gradient(loss_W, self.params['W1']),
-                 'b1': numerical_gradient(loss_W, self.params['b1']),
-                 'W2': numerical_gradient(loss_W, self.params['W2']),
-                 'b2': numerical_gradient(loss_W, self.params['b2'])}
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
 
         return grads
 
